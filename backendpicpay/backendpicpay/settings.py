9afa64558fd1adfac6f67695e2f64640be2bf663
mcarina <marciacarina931@gmail.com>
2024-07-02 14:51:28 -0400

    com docker, gunicorn e nginx

--- backendpicpay/backendpicpay/settings.py
+++ backendpicpay/backendpicpay/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'backendpicpay.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,8 +111,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'  # Alterado para '/static/' ao invés de 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Diretório onde collectstatic irá coletar os arquivos estáticos
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Diretório onde estão os arquivos estáticos do projeto
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
